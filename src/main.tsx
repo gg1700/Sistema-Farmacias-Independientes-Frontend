9b1046e152f562ab990664c7668495604234a69d
gg1700 <gabrielcamacho1700@gmail.com>
2026-07-28 11:44:52 -0400

    feat: implement main application component and update rendering logic

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
+import React from 'react';
 import { createRoot } from 'react-dom/client';
+import App from './App.tsx'
 
 const root = createRoot(document.getElementById('root')!);
 
-root.render(<h1>Home Page</h1>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
